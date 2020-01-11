6e8fa4821077ff083d4d60b5eb428a5ccfc0a95c
taoxingyu <1396563360@qq.com>
2020-01-11 10:26:57 +0800

    提交

---
+++ Project6/源0711.cpp
@@ -0,0 +1,14 @@
+//forÑ­»·Óï¾ä
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int i = 0;
+	int k = 0;
+	for (i = 0,k = 0; k == 0; i++, k++)
+	{
+		k++;
+		printf("hehe\n");
+	}
+	system("pause");
+	return 0;
+}
